291ec13ade192fe1070a3b316042fd6340bf65b4
Nadia <ladarud00@gmail.com>
2025-11-06 17:56:39 +0300

    Add DB_Create.py

--- DB_Create.py
+++ DB_Create.py
@@ -1,2 +1,28 @@
 import psycopg2
 from DB_Settings import Settings
+
+class Create(Settings):
+
+    def create_table_Client(self):
+        with self._cursor() as cur:
+            cur.execute("""
+            create table if not exists Client(
+            id serial primary key,
+            first_name varchar(80) not null,
+            last_name varchar(80) not null,
+            email varchar(200) not null unique
+            );
+            """)
+            self._commit()
+
+
+    def create_table_Phone(self):
+        with self._cursor() as cur:
+            cur.execute("""
+            create table if not exists Phone(
+            id serial primary key,
+            number varchar(20) not null unique,
+            client_id integer not null references Client(id) on delete cascade
+            );
+            """)
+            self._commit()
